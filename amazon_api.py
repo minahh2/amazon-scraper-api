79da5532f6f17700720987639cedeb6940aa8af2
minahh2 <44125858+minahh2@users.noreply.github.com>
2025-11-12 18:30:07 +0200

    Update amazon_api.py

--- amazon_api.py
+++ amazon_api.py
@@ -35,10 +35,11 @@ def scrape():
     config = CrawlerRunConfig(
     cache_mode=CacheMode.BYPASS,
     extraction_strategy=extraction_strategy,
+    js_code="document.querySelector('#aod-ingress-link')?.click();",
     scan_full_page=True,
     scroll_delay=0.3,
     delay_before_return_html=1.0,   
-    #simulate_user=True    
+    simulate_user=True    
 )
 
 
